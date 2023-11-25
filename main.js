88b2d4158510b51bd7d309eb77375d304ddadb2a
snipe <72265661+notsniped@users.noreply.github.com>
2023-11-25 19:46:58 +0530

    Update the isobot website redirect to new website
    The isobot website has been updated, completed, and refreshed completely to give it a nicer look and more informative view.
    
    isobot's new website is now informative and useful enough to be used for the isobot website redirect.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function rd(target) {
     // Redirects the user to a subpage, or another page.
     if (target == "git") {window.location = "https://github.com/orgs/PyBotDevs/repositories";}
-    else if (target == "isobot") {window.location = "https://github.com/PyBotDevs/isobot";}
+    else if (target == "isobot") {window.location = "https://pybotdevs.github.io/isobot";}
     else if (target == "yatodb") {window.location = "https://PyBotDevs.github.io/yatodb";}
     else if (target == "iso6.9") {window.location = "https://github.com/PyBotDevs/iso6.9";}
     else if (target == "skyblockpy") {window.location = "https://github.com/PyBotDevs/skyblockpy";}
